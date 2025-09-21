4ad0896fcdce8689e946d467913434dfc2fb5e99
pdrabik223 <pdrabik223@gmail.com>
2025-09-21 10:21:21 +0200

    still we to often refresh leds

--- src/main.cpp
+++ src/main.cpp
@@ -3,45 +3,34 @@
 
 LedStrip<5> led_strip(16);
 
-unsigned get_random_color()
+const unsigned NO_LEDS = 8;
+const unsigned NO_MOON_COLORS = 8;
+const double MINIMAL_BRIGHTNESS_LEVEL = 0.1;
+const unsigned long DELAY = 60000; // wait 60s
+
+const CRGB moon_colors[NO_MOON_COLORS] = {
+    CRGB(220, 220, 220), // white
+    CRGB(220, 150, 52),  // yellow
+    CRGB(200, 132, 6),   // orange
+    CRGB(220, 0, 0),     // red
+    CRGB(220, 60, 60),   // pink
+    CRGB(149, 66, 245),  // purple
+    CRGB(84, 248, 254),  // light blue
+    CRGB(0, 0, 220),     // blue
+};
+
+const CRGB get_random_color()
 {
 
   if (random(3) != 0)
-    return 0;
+    return moon_colors[0];
   if (random(2) == 0)
-    return random(1, 6);
-  return random(6, 8);
+    return moon_colors[random(1, 6)];
+  return moon_colors[random(6, 8)];
 }
 
-const unsigned NO_LEDS = 8;
-
-const unsigned NO_MOON_COLORS = 8;
-const unsigned long DELAY = 60000; // wait 60s
-
 unsigned last_phase = 2;
-unsigned phase_color_memory[2] = {get_random_color(), get_random_color()};
-
-const CRGB moon_colors[NO_MOON_COLORS] = {
-    CRGB(110, 110, 110), // white
-    CRGB(112, 75, 26),   // yellow
-    CRGB(100, 66, 3),    // orange
-    CRGB(110, 30, 30),   // pink
-    CRGB(110, 0, 0),     // red
-    CRGB(42, 124, 140),  // orange
-    CRGB(151, 64, 168),  // purple
-    CRGB(0, 0, 110),     // blue
-};
-
-const CRGB moon_dimm_colors[NO_MOON_COLORS] = {
-    CRGB(55, 55, 55), // white
-    CRGB(56, 36, 13), // yellow
-    CRGB(50, 33, 2),  // orange
-    CRGB(55, 15, 15), // pink
-    CRGB(55, 0, 0),   // red
-    CRGB(21, 75, 70), // orange
-    CRGB(75, 32, 84), // purple
-    CRGB(0, 0, 55),   // blue
-};
+CRGB phase_color_memory[2] = {get_random_color(), get_random_color()};
 
 const unsigned used_leds[NO_LEDS] = {
     15, // new
@@ -55,54 +44,135 @@ const unsigned used_leds[NO_LEDS] = {
 
 };
 
-const unsigned long moon_lunar_cycle_minutes = 42524046;
-
-const unsigned long moon_lunar_segment_minutes = moon_lunar_cycle_minutes / 16;
-
-unsigned long current_moon_lunar_cycle_minutes = 37771951;
-
-const unsigned get_phase()
+const CRGB dimm(const CRGB &original_color, double brightness_level = 1)
 {
-  unsigned phase = 0;
-  unsigned long current_moon_lunar_cycle_minutes_copy = current_moon_lunar_cycle_minutes;
-  while (1 < 2)
-  {
-    if (current_moon_lunar_cycle_minutes_copy < moon_lunar_segment_minutes)
-      break;
-    current_moon_lunar_cycle_minutes_copy -= moon_lunar_segment_minutes;
-    phase++;
-  }
-  return phase;
-}
 
-void update_leds(const unsigned segment)
+  if (brightness_level <= MINIMAL_BRIGHTNESS_LEVEL)
+    return CRGB(original_color.red * MINIMAL_BRIGHTNESS_LEVEL,
+                original_color.green * MINIMAL_BRIGHTNESS_LEVEL,
+                original_color.blue * MINIMAL_BRIGHTNESS_LEVEL);
+
+  // brightness_level = int(brightness_level * 100) / 100;
+
+  // Serial.println(brightness_level);
+
+  return CRGB(original_color.red * brightness_level,
+              original_color.green * brightness_level,
+              original_color.blue * brightness_level);
+};
+
+void reset_led_colors()
 {
-  if (last_phase == segment)
-    return;
-
-  last_phase = segment;
   led_strip.Clear();
 
   for (uint32_t j = 0; j < NO_LEDS; j++)
   {
-    led_strip.Set(used_leds[j], CRGB(10, 10, 10));
+    led_strip.Set(used_leds[j], dimm(moon_colors[0], MINIMAL_BRIGHTNESS_LEVEL));
+  }
+}
+
+void test_colors()
+{
+
+  while (1 < 2)
+  {
+
+    for (uint32_t i = 0; i < NO_MOON_COLORS; i++)
+    {
+      led_strip.Clear();
+
+      for (uint32_t j = 0; j < NO_LEDS; j++)
+      {
+        led_strip.Set(used_leds[j], dimm(moon_colors[i], 1));
+      }
+
+      led_strip.Update();
+      delay(500);
+    }
+  }
+}
+
+const unsigned long moon_lunar_cycle_minutes = 42524;
+
+unsigned long current_moon_lunar_cycle_minutes = 37719;
+
+unsigned previous_moon_phase = -1;
+
+double get_moon_angle()
+{
+  return double(current_moon_lunar_cycle_minutes) / double(moon_lunar_cycle_minutes);
+}
+
+unsigned print_me = 0;
+unsigned prev_moon_phase = 2;
+
+struct Phase
+{
+
+  unsigned current_moon_phase;
+  double dimm_level;
+
+  Phase(double position)
+  {
+
+    current_moon_phase = int(position * 8);
+    dimm_level = ((position * 8) - current_moon_phase);
+    // int val = dimm_level * 1000;
+    // dimm_level = val / 1000;
+    // Serial.println(dimm_level, 8);
+    // dimm_level = ((int)(dimm_level * 100) / 100);
+
+    if (current_moon_phase != prev_moon_phase)
+    {
+      prev_moon_phase = current_moon_phase;
+      phase_color_memory[0] = phase_color_memory[1];
+      phase_color_memory[1] = get_random_color();
+    }
+  }
+  const Phase &operator=(const Phase &other)
+  {
+
+    this->current_moon_phase = other.current_moon_phase;
+    this->dimm_level = other.dimm_level;
+    return *this;
+  }
+  const unsigned get_current_phase() const
+  {
+    return current_moon_phase;
   }
 
-  if (segment % 2 == 0)
+  const unsigned get_next_phase() const
   {
-    led_strip.Set(used_leds[segment / 2], moon_colors[phase_color_memory[0]]);
+    return (current_moon_phase + 1) % 8;
   }
-  else
-  {
-    led_strip.Set(used_leds[segment / 2], moon_dimm_colors[phase_color_memory[0]]);
-    if ((segment / 2) + 1 < NO_LEDS)
-      led_strip.Set(used_leds[(segment / 2) + 1], moon_dimm_colors[phase_color_memory[1]]);
-    else
-      led_strip.Set(used_leds[0], moon_dimm_colors[phase_color_memory[1]]);
 
-    phase_color_memory[0] = phase_color_memory[1];
-    phase_color_memory[1] = get_random_color();
+  const CRGB get_current_color() const
+  {
+    return dimm(phase_color_memory[0], 1 - dimm_level);
   }
+
+  const CRGB get_next_color() const
+  {
+    return dimm(phase_color_memory[1], dimm_level);
+  }
+};
+
+Phase previous_phase = Phase(0.0);
+
+void update_leds(const Phase &phase)
+{
+  if (phase.current_moon_phase == previous_phase.current_moon_phase && phase.dimm_level == previous_phase.dimm_level)
+  {
+    Serial.println("ignore");
+    return;
+  }
+  Serial.println("update");
+  previous_phase = phase;
+
+  reset_led_colors();
+
+  led_strip.Set(used_leds[phase.get_current_phase()], phase.get_current_color());
+  led_strip.Set(used_leds[phase.get_next_phase()], phase.get_next_color());
 
   for (uint32_t j = 0; j < NO_LEDS; j++)
   {
@@ -114,22 +184,20 @@ void update_leds(const unsigned segment)
 
 void setup()
 {
-  randomSeed(0);
-
-  for (uint32_t j = 0; j < NO_LEDS; j++)
-  {
-    led_strip.Set(used_leds[j], CRGB(10, 10, 10));
-  }
+  randomSeed(45);
+  Serial.begin(9600);
+  reset_led_colors();
   led_strip.Update();
 }
+
 void loop()
 {
   // test_colors();
-  auto phase = get_phase();
-  update_leds(phase);
+  // auto phase = get_phase();
+  update_leds(Phase(get_moon_angle()));
 
-  delay(DELAY);
-  current_moon_lunar_cycle_minutes += 1;
+  // delay(1); // 600 x the speed
+  ++current_moon_lunar_cycle_minutes;
 
   if (current_moon_lunar_cycle_minutes >= moon_lunar_cycle_minutes)
     current_moon_lunar_cycle_minutes -= moon_lunar_cycle_minutes;
