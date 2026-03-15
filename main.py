6ede13fd3765adcb54aaeaee2180b1d1becf54d4
pdrabik223 <pdrabik223@gmail.com>
2026-03-15 04:55:06 +0100

    Animation with timeout

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from math import floor
 import random
 import time
 import utime
@@ -10,8 +11,6 @@ import ntptime
 from machine import RTC
 import _thread
 
-
-# 
 NEW_MOON = 15 #ok
 WAXING_CRESCENT = 13
 FIRST_QUARTER = 11
@@ -44,12 +43,17 @@ moon_colors_rgb = [
     Color(255, 182, 193),  # pink – light
     Color(255, 105, 180)   # pink – hot pink
 ]
+
+
 led_strip = LedStrip(16, 22)
 app = App(hostname="lunar_calendar.local")
 rtc = RTC()
 
-
-brightness = 255
+is_animation_on = False
+animation_timeout = 5 # in minutes
+animation_timer = 0
+brightness = 100
+brightness_night_mode = 50
 moon_cycle_length_days = 29.53059
 known_moon_phase_and_time = (
 0, # index of the phase in moon phases array, corresponds to New Moon 
@@ -61,7 +65,7 @@ led_color = Color.white()
 is_color_random = False
 is_night_mode = False
 
-current_phase = 4
+current_phase = 1
 
 night_mode_start = (0,0,0,23,59,0,0,0)
 night_mode_end = (0,0,0,5,30,0,0,0)
@@ -115,7 +119,7 @@ def get_operation_time(uptime:int):
     if uptime < 24 * 60:
         return f"{uptime // 60}h {uptime%60}min"
     
-    return f"{uptime // (24 * 60)}days {uptime // 60}h {uptime%60}min"
+    return f"{(uptime // (24 * 60))}days {(uptime % (24 * 60)) // 60}h {floor(uptime%60)}min"
 
 
 def favicon(cl: socket.socket, parameters: dict):
@@ -126,13 +130,17 @@ def home_page(cl: socket.socket, parameters: dict):
     "local_time" : time_str(),
     "lunar_phase" : moon_phases_string[current_moon_phase_id()],
     "moon_age": (datetime_diff_seconds(utime.localtime(), known_moon_phase_and_time[1]) / seconds_in_day) % moon_cycle_length_days,
-    "uptime":get_operation_time(uptime_minutes),
+    "animation_timeout":animation_timeout,
+    "uptime":uptime_minutes,
     "brightness" : brightness,
     "color" : get_hex_color(led_color),
+    "night_mode_start":get_hour_and_minute(night_mode_start),
     "is_color_random":  "checked" if is_color_random else "",
     "is_night_mode_on": "checked" if is_night_mode else "",
     "night_mode_start":get_hour_and_minute(night_mode_start),
     "night_mode_end":get_hour_and_minute(night_mode_end),
+    "night_mode_brightness" : brightness_night_mode
+    
     }
 )))
     
@@ -144,11 +152,19 @@ def save_settings(cl: socket.socket, parameters: dict):
     global is_night_mode
     global night_mode_start
     global night_mode_end
-        
+    global brightness_night_mode
+    global animation_timeout
+    
     brightness = int(parameters.get('brightness', brightness))
-
     if brightness < 0: brightness = 0
     if brightness > 255: brightness = 255
+    
+    brightness_night_mode = int(parameters.get('nightModeBrightness', brightness_night_mode))
+    if brightness_night_mode < 0: brightness_night_mode = 0
+    if brightness_night_mode > 255: brightness_night_mode = 255
+    
+    animation_timeout = int(parameters.get('animationTimeout', animation_timeout))
+    if animation_timeout < 0: animation_timeout = 0
     
     is_color_random = parameters.get('isColorRandom', 
                                      "true" if is_color_random else "false") == "true"
@@ -164,7 +180,7 @@ def save_settings(cl: socket.socket, parameters: dict):
     
     night_mode_end = parse_hour_and_minute(parameters.get("nightModeEnd", get_hour_and_minute(night_mode_end)))
     
-    display_moon_phase(moon_phases[current_moon_phase_id()])
+    update_leds()
     cl.sendall(compose_response())
 
 
@@ -189,24 +205,52 @@ def night_mode():
     
     return False
 
-def display_moon_phase(phase: int):
+def loading_animation(phase: int):
     global led_color
     global brightness
     global current_phase
-    global is_color_random
+     
+     
+    led_brightness = brightness
     
-    led_strip.fill(Color.black())
-    
-    
+    if night_mode():
+        led_brightness = brightness_night_mode
+
     if current_phase != phase:
         current_phase = phase
         if is_color_random:
             led_color = random.choice(moon_colors_rgb)
+            
+
+    led_strip.fill(Color.black())
+    
+    for i in range(moon_phases.index(phase), len(moon_phases) + moon_phases.index(phase)):
+        led_strip.set_pixel(moon_phases[i % len(moon_phases)], led_color, led_brightness)
+        time.sleep(0.1)
+        
+
+def display_moon_phase(phase: int):
+    global led_color
+    global brightness
+    global current_phase
+     
+     
+    led_brightness = brightness
+    
     if night_mode():
-        return
+        led_brightness = brightness_night_mode
+
+
+    led_strip.fill(Color.black())
     
+
+    if current_phase != phase:
+        current_phase = phase
+        if is_color_random:
+            led_color = random.choice(moon_colors_rgb)
+            
     
-    led_strip.set_pixel(phase, led_color, brightness)
+    led_strip.set_pixel(phase, led_color, led_brightness)
 
 
 def datetime_diff_seconds(timestamp_a,timestamp_b):
@@ -226,6 +270,7 @@ def synch_time(rtc, timezone_offset = 1):
     tm[6] + 1,        # weekday (1–7)
     tm[3], tm[4], tm[5], 0
 ))
+    
 lunar_phases = [
     (0, 1),
     (1, 6.38),
@@ -237,7 +282,6 @@ lunar_phases = [
     (23.15, 28.53),
     (28.54, 29.53)
 ]
-
 def get_phase_id(lunar_age):
  
     for id, (start, end) in enumerate(lunar_phases):
@@ -252,26 +296,56 @@ def current_moon_phase_id():
     
     return (known_moon_phase_and_time[0] + offset) % len(moon_phases)
 
-
+def update_leds():
+    global animation_timeout
+    global animation_timer
+    global is_animation_on
+    
+    if (animation_timer < animation_timeout ):  
+        if (not is_animation_on):
+            loading_animation(moon_phases[current_moon_phase_id()])
+        display_moon_phase(moon_phases[current_moon_phase_id()])
+        is_animation_on = True
+    else:
+        led_strip.fill(Color.black())
+        is_animation_on = False
+        
 def animation():
     global uptime_minutes
+    global animation_timer
     while(1<2):
-        for _ in range(24 * 60):
-            display_moon_phase(moon_phases[current_moon_phase_id()])
-            time.sleep(60)
-            uptime_minutes+=1
+        for _ in range(24 * 60 * 60):
+            update_leds()
+            time.sleep(1)
+            
+            uptime_minutes += 1 / 60
+            animation_timer += 1
+            
         synch_time(rtc) # once a day synch time of the 
+
+def run_animation(cl: socket.socket, parameters: dict):
     
-    
+    global animation_timer
+    animation_timer = 0
+    cl.sendall(compose_response())
+
+
+def status(cl: socket.socket, parameters: dict):
+    cl.sendall(compose_response())
+
+
 if __name__ == "__main__":
     # print(get_hex_color(led_color))
     synch_time(rtc)
     random.seed(utime.localtime()[4] * 60 + utime.localtime()[5])
-    
     _thread.start_new_thread(animation, ())
+    
     app.register_endpoint("/v1", home_page)
     app.register_endpoint("/v1/save_settings", save_settings)
-    app.register_endpoint("/favicon.ico", save_settings)
+    app.register_endpoint("/v1/run_animation", run_animation)
+    app.register_endpoint("/v1/status", status)
+    # app.register_endpoint("/favicon.ico", save_settings)
+
 
     try:
         app.main_loop()
